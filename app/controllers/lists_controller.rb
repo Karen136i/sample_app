0766e0aecb97770d1426d399b034e0b274178211
EC2 Default User <156569903+Karen136i@users.noreply.github.com>
2024-02-08 12:04:19 +0000

    test

--- app/controllers/lists_controller.rb
+++ app/controllers/lists_controller.rb
@@ -4,14 +4,18 @@ class ListsController < ApplicationController
     @list = List.new
   end
 
-  #以下を追加
   def create
-    # 1,&2, データを受け取り新規登録するためンおインスタンス作成
-    list = List.new(list_params)
-    #3.データをデータベースに保存するためのsaveメソッド実行
-    list.save
-    #4.　トップ画面へリダイレクト
-    redirect_to list_path(list.id)
+    # １. データを受け取り新規登録するためのインスタンス作成
+    @list = List.new(list_params)
+    # 2. データをデータベースに保存するためのsaveメソッド実行
+    if @list.save
+      flash[:notice] = "投稿に成功しました。"
+      redirect_to list_path(@list.id)
+    else
+       # 3. フラッシュメッセージを定義し、詳細画面へリダイレクト
+      flash.now[:alert] = "投稿に失敗しました。"
+      render :new
+    end
   end
 
   def index
@@ -32,6 +36,11 @@ class ListsController < ApplicationController
     redirect_to list_path(list.id)
   end
 
+  def destroy
+    list = List.find(params[:id])  # データ（レコード）を1件取得
+    list.destroy  # データ（レコード）を削除
+    redirect_to '/lists'  # 投稿一覧画面へリダイレクト
+  end
 
   private
   #ストロングパラメータ
